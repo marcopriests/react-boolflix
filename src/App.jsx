38c9aaed328e716cc417d7c01d29ebe36312f91b
marcopriests <prtmrc98@gmail.com>
2025-05-20 12:33:19 +0200

    [UPD] risultati stampati in pagina sotto forma di elenchi

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import axios from "axios"
 
 function App() {
   const [search, setSearch] = useState('')
+  const [data, setData] = useState(null)
 
   const handleChange = (e) => {
     setSearch(e.target.value)
@@ -11,25 +12,36 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault()
     getData()
-    console.log('cliccato')
   }
 
   const getData = () => {
     const searchValue = search
 
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=98e906b1055207ca319b06893208305a&query=${searchValue}`).then(res => {
-      console.log(searchValue)
-      console.log(res.data.results)
+      setData(res.data.results)
     })
   }
 
   return (
     <>
       <div className="container p-5">
-        <form onSubmit={handleSubmit} className="input-group">
+        {/* ------------------------------------------------INPUT SEARCH */}
+        <form onSubmit={handleSubmit} className="input-group mb-5">
           <input type="text" className="form-control" placeholder="Search..." onChange={handleChange} />
           <button type="submit" className="btn btn-danger">Search</button>
         </form>
+
+        {/* --------------------------------------------------ELENCO FILM */}
+        {!data
+          ? <div>Nothing to show...</div>
+          : data.map(elem => {
+            return <ul className="list-group list-unstyled mb-3" key={elem.id}>
+              <li className="list-group-item"><h4 className="fw-semibold">{elem.title}</h4></li>
+              <li className="list-group-item"><h5 className="fw-semibold fst-italic">{elem.original_title}</h5></li>
+              <li className="list-group-item">{elem.original_language}</li>
+              <li className="list-group-item fw-semibold">{elem.vote_average}</li>
+            </ul>
+          })}
       </div>
     </>
   )
