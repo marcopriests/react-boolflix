9aa108dda64f2ca6d62fad8752d2a21a8f15679c
marcopriests <prtmrc98@gmail.com>
2025-05-20 14:35:49 +0200

    [UPD] voti con stelline fatti

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,16 +2,28 @@ import React from 'react'
 
 const MovieCard = ({ movie }) => {
     const lang = movie.original_language == 'en' ? 'gb' : movie.original_language
+
+    const vote = Math.round(movie.vote_average / 2)
+    const stars = []
+
+    for (let i = 0; i < 5; i++) {
+        if (vote > i) {
+            stars.push(<i className="fa-solid fa-star" key={i}></i>)
+        } else {
+            stars.push(<i className="fa-regular fa-star" key={i}></i>)
+        }
+
+    }
     return (
         <div className='col-6 col-md-4 col-lg-3 mb-3'>
             <div className="card">
-                <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="" className="card-img-top" />
+                <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="Image not found" className="card-img-top" />
                 <div className="card-body">
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item"><h4 className="fw-semibold">{movie.title}</h4></li>
                         <li className="list-group-item"><h5 className="fw-semibold fst-italic">{movie.original_title}</h5></li>
                         <li className="list-group-item"><i className={`fi fi-${lang}`}></i></li>
-                        <li className="list-group-item fw-semibold">{movie.vote_average}</li>
+                        <li className="list-group-item fw-semibold">{stars}</li>
                     </ul>
                 </div>
             </div>
--- src/components/TvCard.jsx
+++ src/components/TvCard.jsx
@@ -2,16 +2,28 @@ import React from 'react'
 
 const TvCard = ({ tv }) => {
     const lang = tv.original_language == 'en' ? 'gb' : tv.original_language
+
+    const vote = Math.round(tv.vote_average / 2)
+    const stars = []
+
+    for (let i = 0; i < 5; i++) {
+        if (vote > i) {
+            stars.push(<i className="fa-solid fa-star" key={i}></i>)
+        } else {
+            stars.push(<i className="fa-regular fa-star" key={i}></i>)
+        }
+
+    }
     return (
         <div className='col-6 col-md-4 col-lg-3 mb-3'>
             <div className="card">
-                <img src={`https://image.tmdb.org/t/p/original${tv.poster_path}`} alt="" className="card-img-top" />
+                <img src={`https://image.tmdb.org/t/p/original${tv.poster_path}`} alt="Image not found" className="card-img-top" />
                 <div className="card-body">
                     <ul className="list-group list-group-flush mb-3">
                         <li className="list-group-item"><h4 className="fw-semibold">{tv.name}</h4></li>
                         <li className="list-group-item"><h5 className="fw-semibold fst-italic">{tv.original_name}</h5></li>
                         <li className="list-group-item"><i className={`fi fi-${lang}`}></i></li>
-                        <li className="list-group-item fw-semibold">{tv.vote_average}</li>
+                        <li className="list-group-item fw-semibold">{stars}</li>
                     </ul>
                 </div>
             </div>
