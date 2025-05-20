e28507f71651531a765e7c03f458ce0145190432
marcopriests <prtmrc98@gmail.com>
2025-05-20 12:10:51 +0200

    [UPD] input search creato

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-
+import { useState } from "react"
 
 function App() {
+  const [search, setSearch] = useState('')
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('cliccato')
+  }
 
   return (
     <>
-
+      <div className="container p-5">
+        <form onSubmit={handleSubmit} className="input-group">
+          <input type="text" className="form-control" placeholder="Search..." onChange={handleChange} />
+          <button type="submit" className="btn btn-danger">Search</button>
+        </form>
+      </div>
     </>
   )
 }
